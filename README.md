c4591ee47a9ae99ada518f7a595d08e670eeae55
Mohamad Wazzeh <14247647+mwazzeh@users.noreply.github.com>
2021-05-30 16:28:23 -0400

    add grid search for the hyper parameter optimization in federated continuous

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # localfed
+Federated learning for continuous authentication
+
+ca>exp>main
+        Will contain the main command to run FL CA
+
+datasets>make_dataset
+        Create a custom dataset
+
+datasets>pickles
+        Contains pickle files created from make_dataset
+
+src>data>DataGenerator
+        use a data_provider to generate data containers based on specific distribution 
+        load and save pickle files
--- experiments/federated_averaging.py
+++ experiments/federated_averaging.py
@@ -1,22 +1,23 @@
 import logging
+import pickle
 
 from torch import nn
 
 import src
-from libs.model.cv.cnn import CNN_OriginalFedAvg
-from src.federated.components import testers, client_selectors, aggregators, optims, trainers
+from components import client_selectors, aggregators, trainers, testers, optims
+from components.trainers import CPUTrainer
 from libs.model.linear.lr import LogisticRegression
-from src.data.data_provider import PickleDataProvider
+from src.data.data_provider import PickleDataProvider, LocalMnistDataProvider
 from src.federated import plugins
 from src.data.data_generator import DataGenerator
 from src.federated.federated import Events
 from src.federated.federated import FederatedLearning
-from src.federated.trainer_manager import TrainerManager
+from src.federated.trainer_manager import TrainerManager, ADVTrainer
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('main')
 
-data_file = '../datasets/pickles/70_2_600_big_mnist.pkl'
+data_file = '../datasets/pickles/2_50_medium_shards.pkl'
 test_file = '../datasets/pickles/test_data.pkl'
 
 logger.info('Generating Data --Started')
@@ -27,27 +28,33 @@ client_data = dg.distributed
 dg.describe()
 logger.info('Generating Data --Ended')
 
-trainer_manager = TrainerManager(trainers.CPUTrainer, batch_size=50, epochs=20, criterion=nn.CrossEntropyLoss(),
+trainer_manager = TrainerManager(trainers.CPUTrainer, batch_size=8, epochs=10, criterion=nn.CrossEntropyLoss(),
                                  optimizer=optims.sgd(0.1))
+
+
+def create_model():
+    return LogisticRegression(28 * 28, 10)
+
 
 federated = FederatedLearning(
     trainer_manager=trainer_manager,
     aggregator=aggregators.AVGAggregator(),
-    tester=testers.Normal(batch_size=50, criterion=nn.CrossEntropyLoss()),
-    client_selector=client_selectors.Random(3),
+    tester=testers.Normal(batch_size=8, criterion=nn.CrossEntropyLoss()),
+    # client_selector=client_selectors.Random(10),
+    client_selector=client_selectors.All(),
     trainers_data_dict=client_data,
-    # initial_model=lambda: LogisticRegression(28 * 28, 10),
-    initial_model=lambda: CNN_OriginalFedAvg(),
-    num_rounds=0,
+    initial_model=create_model,
+    num_rounds=10,
     desired_accuracy=0.99
 )
 
-federated.plug(plugins.FederatedLogger([Events.ET_TRAINER_SELECTED, Events.ET_ROUND_FINISHED]))
-federated.plug(plugins.FederatedTimer([Events.ET_TRAINER_FINISHED]))
+federated.plug(plugins.FederatedLogger([Events.ET_ROUND_FINISHED, Events.ET_TRAINER_SELECTED]))
+federated.plug(plugins.FederatedTimer([Events.ET_ROUND_START, Events.ET_TRAIN_END]))
+federated.plug(plugins.FL_CA())
 # federated.plug(plugins.FedPlot())
 # federated.plug(plugins.CustomModelTestPlug(PickleDataProvider(test_file).collect().as_tensor(), 8))
 # federated.plug(plugins.FedSave())
-# federated.plug(plugins.WandbLogger(config={'num_rounds': 10}))
+federated.plug(plugins.WandbLogger(config={'num_rounds': 10}))
 
 logger.info("----------------------")
 logger.info("start federated 1")
